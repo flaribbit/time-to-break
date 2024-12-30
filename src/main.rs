5f44d89cdadafd2e50fe1bb24a091bd2a23b21be
flaribbit <1149761294@qq.com>
2024-12-30 19:13:24 +0800

    用COM了

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use enigo::{Key, KeyboardControllable};
+use windows::{
+    core::*,
+    Win32::System::Com::{CoCreateInstance, CoInitialize, CLSCTX_ALL},
+    Win32::UI::Shell::{IShellDispatch4, Shell},
+};
 
-fn toggle_desktop(enigo: &mut enigo::Enigo) {
-    enigo.key_down(Key::Meta);
-    enigo.key_down(Key::D);
-    enigo.key_up(Key::D);
-    enigo.key_up(Key::Meta);
+fn toggle_desktop(shell: &IShellDispatch4) -> Result<()> {
+    unsafe { shell.ToggleDesktop() }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let interval = std::env::args()
         .nth(1)
         .map(|s| s.parse::<u64>().unwrap())
         .unwrap_or(20);
-    let mut enigo = enigo::Enigo::new();
-    println!("This program will press Win+D every {} minutes", interval);
+    let shell: IShellDispatch4 = unsafe {
+        CoInitialize(None).ok()?;
+        CoCreateInstance(&Shell, None, CLSCTX_ALL)?
+    };
+    println!(
+        "This program will toggle desktop every {} minutes",
+        interval
+    );
     loop {
         std::thread::sleep(std::time::Duration::from_secs(interval * 60));
-        toggle_desktop(&mut enigo);
+        toggle_desktop(&shell)?;
     }
 }
